37f670f48cb95ad492cb4076f7241a91b5c365de
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-09-22 16:05:53 -0300

    usando a api string

---
+++ string_api_1.js
@@ -0,0 +1,14 @@
+// length: Retorna o tamanho da string
+console.log("JavaScript".length)
+
+// indexOf: Retorna a primeira posição encontrada do caractere passado por parâmetro
+console.log("PHP".indexOf("P"))
+
+// lastIndexOf: Retorna a última posição encontrada do caractere passado por parâmetro
+console.log("PHP".lastIndexOf("P"))
+
+// toUpperCase: Retorna uma nova String convertendo as letras para maiúsculas
+console.log("cobol".toUpperCase())
+
+// toLowerCase: Retorna uma nova String convertendo as letras para minúsculas
+console.log("ALGOL".toLowerCase())
