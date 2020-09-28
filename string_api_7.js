5fbd8c6790bd90e22a95ac4d42090faaff131acc
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-09-28 15:44:18 -0300

    usos de concat, padstart, padend, repeat e trim

---
+++ string_api_7.js
@@ -0,0 +1,22 @@
+// concat: Retorna uma nova String resultante da concatenação da que está invocando a
+// função e da outra, passada por parâmetro
+console.log("Java".concat("Script"))
+console.log("Data".concat("Flex"))
+
+// padStart: Completa a String com caracteres no início
+console.log("Script".padStart(10, "Java"))
+
+// padEnd: Completa a String com caracteres no fim
+console.log("C".padEnd(3, "+"))
+
+// repeat: Repete um caractere
+console.log("C".concat("+".repeat(2)))
+
+// trim: Elimina espaços em branco no início e no fim
+console.log(" Self ".trim())
+
+// trimLeft: Elimina espaços em branco no início
+console.log(" Scheme ".trimLeft())
+
+// trimRight: Elimina espaços em branco no fim
+console.log(" Perl ".trimRight())
