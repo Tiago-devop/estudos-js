fc15bc5688fdf072bbe89c8295ad8e627b99fe3e
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-09-28 22:33:07 -0300

    transformando dados em formato json

---
+++ json_1.js
@@ -0,0 +1,6 @@
+console.log(JSON.stringify(10))
+console.log(JSON.stringify('JavaScript'))
+console.log(JSON.stringify(true))
+console.log(JSON.stringify({ name: 'Selfs', paradigm: 'OO' }))
+console.log(JSON.stringify([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+console.log(JSON.stringify(null))
