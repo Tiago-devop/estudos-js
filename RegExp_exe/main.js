25f52faec03a9c22db54dd45537ed8dc62d0b4c3
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-09-28 23:22:33 -0300

    usos do JSON

--- RegExp_exe/main.js
+++ RegExp_exe/main.js
@@ -4,5 +4,19 @@ const parsedStatement = statement.match(regexp)
 const tableName = parsedStatement[1]
 let columns = parsedStatement[2]
 columns = columns.split(", ")
-console.log(tableName)
-console.log(columns)
+const database = {
+    tables: {
+        [tableName]: {
+            columns: {},
+            data: []
+        }
+    }
+}
+
+for (let column of columns) {
+    column = column.trim().split(' ')
+    const name = column[0]
+    const type = column[1]
+    database.tables[tableName].columns[name] = type
+}
+console.log(JSON.stringify(database, undefined, ' '))
