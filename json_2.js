31f6011de0a6a748cd33bfc782b831025a07a90e
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-09-28 22:38:42 -0300

    convertendo json para determinado tipo de dado

---
+++ json_2.js
@@ -0,0 +1,7 @@
+// O método JSON.parse converte um JSON para um determinado tipo de dado
+console.log(JSON.parse('10'))
+console.log(JSON.parse('"JavaScript"'))
+console.log(JSON.parse('true'))
+console.log(JSON.parse('false'))
+console.log(JSON.parse('{"name": "Self", "paradigm": "OO"}'))
+console.log(JSON.parse('null'))
