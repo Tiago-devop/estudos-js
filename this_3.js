71b298214ba5227fd087c8d1d3a12574a309c3cb
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-09-29 10:48:29 -0300

    usando bind

---
+++ this_3.js
@@ -0,0 +1,10 @@
+// A operação bind permite encapsular o this dentro da função, retornando-a
+const calculateArea = function(fn) {
+    return fn(Math.PI * Math.pow(this.radius, 2))
+}
+const circle = {
+    radius: 10,
+    calculateArea
+}
+const calculateAreaForCircle = calculateArea.bind(circle)
+console.log(calculateAreaForCircle(Math.round))
