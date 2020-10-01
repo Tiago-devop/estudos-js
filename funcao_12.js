3c36df5e1d3020f9e4566735c9946049c335acff
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-01 15:24:32 -0300

    scope chain

---
+++ funcao_12.js
@@ -0,0 +1,16 @@
+// O execution Context é o ambiente onde o código é executado, sendo composto pelo
+// variable object, scope chain e this
+
+// Dentro de uma função é possivel acessar variáveis existentes fora 
+// dela, por meio da scope chain
+
+const v1 = 10
+const fn1 = function () {
+    const v1 = 100
+    const fn2 = function () {
+        const v1 = 1000
+        console.log(v1)
+    }
+    fn2()
+}
+fn1()
--- funcao_9.js
+++ funcao_9.js
@@ -1,13 +1,13 @@
 // Resumindo, closure é uma função com um scope chain estático que é definida no momento
 // em que a função é criada, por isso, todas as funções na linguagem JavaScript são closures
 
-let v1 = 10
+const v1 = 10
 function fn1() {
     console.log(v1)
 }
 
 function fn2(fn1) {
-    v1 = 100
+    const v1 = 100
     fn1()
 }
 
