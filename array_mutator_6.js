f83d236612742489418a2d7ec3a6000cf75721b5
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-02 18:35:32 -0300

    uso de fill

---
+++ array_mutator_6.js
@@ -0,0 +1,6 @@
+// Os mutator methods quando invocados modificam o array
+
+// fill Preenche os elementos de acordo com a posição de início e fim
+const languages = ['Python', 'C', 'Java']
+languages.fill('JavaScript', 0, 2)
+console.log(languages)
