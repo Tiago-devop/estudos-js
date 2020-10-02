d54a86661b5661eb48ae5d7111c4e2af73358d24
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-02 16:35:25 -0300

    referencia de objeto dentro de objeto

---
+++ destructuring_5.js
@@ -0,0 +1,13 @@
+// Também podemos referenciar as propriedades de 
+// objetos que estão dentro de outros objetos
+
+const language = {
+    name: 'C',
+    author: 'Dennis Ritchie',
+    year: 1972,
+    company: {
+        name: 'Bell Labs'
+    }
+}
+const { name: n, author: a, year: y, company: { name: c } } = language
+console.log(n, a, y, c)
