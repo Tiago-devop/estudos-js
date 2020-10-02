1327a6052282b5a0399da30f20268b80cf974569
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-02 18:30:36 -0300

    usando o reverse

---
+++ array_mutator_5.js
@@ -0,0 +1,6 @@
+// Os mutator methods quando invocados modificam o array
+
+// reverse Inverte a ordem dos elementos
+const languages = ['Python', 'C', 'Java']
+console.log(languages)
+console.log(languages.reverse())
