23301c5358fcccd451b877e3b513cbb75847bfc6
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-02 16:52:01 -0300

    uso de push e pop

---
+++ array_mutator_1.js
@@ -0,0 +1,12 @@
+// Os mutator methods quando invocados modificam o array
+
+// push insere um elemento no final
+const languages = ['Python', 'C', 'Java']
+console.log(languages)
+console.log(languages.push('Ruby'))
+console.log(languages.push('Go'))
+
+// pop remove um elemento do final
+console.log(languages.pop())
+console.log(languages.pop())
+console.log(languages)
