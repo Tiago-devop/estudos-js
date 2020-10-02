6dc69547ddd23e7ac7ab1a2560d2b8804a877728
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-02 16:16:49 -0300

    estraindo de objeto

---
+++ destructuring_3.js
@@ -0,0 +1,10 @@
+// Para extrair os valores de um objeto é necessário
+// referenciar a chave de cada uma das propriedades
+
+const language = {
+    name: 'C',
+    author: 'Dennis Ritchie',
+    year: 1972
+}
+const { name, author, year } = language
+console.log(name, author, year)
