8a178d31d815e53690a3b77f16f4ef865e03fa6c
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-03 11:35:21 -0300

    uso do forEach

---
+++ array_iteration_1.js
@@ -0,0 +1,5 @@
+// Os iteration methods quando invocados iteram sobre os elementos do array
+
+// forEach Executa a função passada por parâmetro para cada elemento
+const frameworks = ['Angular.js', 'React.js', 'Vue.js']
+frameworks.forEach((frameworks) => console.log(frameworks))
