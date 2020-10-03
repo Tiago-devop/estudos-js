38da6393cd8891987ee5d17d02424bf104058ff2
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-03 13:11:55 -0300

    correção

--- array_iteration_5.js
+++ array_iteration_5.js
@@ -2,10 +2,6 @@
 
 // map Retorna um novo array com base no retorno da função passada por parâmetro
 
-numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-const result = numeros.map(numero => {
-    if (numero % 2 === 0) {
-        console.log(numero)
-    }
-})
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const result = numeros.map(numero => numero * 2)
+console.log(result)
