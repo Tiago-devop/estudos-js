56b263086c8ff752a2cd85f0205a49fc81ebb375
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-05 10:47:50 -0300

    uso de concat

---
+++ array_accessor_3.js
@@ -0,0 +1,8 @@
+// Os accessor methods quando invocados retornam 
+// informações específicas sobre o array
+
+// concat Retorna um novo array resultante da 
+// concatenação de um ou mais arrays
+const numb = [1, 2, 3, 4, 5]
+const numb2 = [6, 7, 8, 9, 10]
+console.log(numb.concat(numb2))
