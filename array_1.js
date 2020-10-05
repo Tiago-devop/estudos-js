813f4e2cca0119eaa349b487a51d0583e893a5c8
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-05 11:08:41 -0300

    array

---
+++ array_1.js
@@ -0,0 +1,9 @@
+// Um Array é apenas um objeto que oferece operações
+//  para acessar e manipular suas propriedades
+
+const languages = ['Python', 'C', 'Java']
+const funcconstrutora = new Array('Go', 'Rust', 'Php')
+console.log(funcconstrutora)
+console.log(languages)
+console.log(typeof languages)
+console.log(languages instanceof Array)
