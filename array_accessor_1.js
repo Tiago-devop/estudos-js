a0fab1f8c57c6c05f9cbeb5cd5171f97b6c2bd4a
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-05 10:37:29 -0300

    uso indexof

---
+++ array_accessor_1.js
@@ -0,0 +1,10 @@
+// Os accessor methods quando invocados retornam 
+// informações específicas sobre o array
+
+// indexOf Retorna a posição do primeiro elemento encontrado
+// lastIndexOf Retorna a posição do último elemento encontrado
+
+const languages = ['Python', 'C', 'C', 'Java']
+console.log(languages.indexOf('C'))
+console.log(languages.lastIndexOf('C'))
+console.log(languages.indexOf('JavaScript'))
