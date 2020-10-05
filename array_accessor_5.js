3f86344013282c4b9732309ad51b72681ab778de
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-05 11:00:13 -0300

    uso do join

---
+++ array_accessor_5.js
@@ -0,0 +1,7 @@
+// Os accessor methods quando invocados retornam 
+// informações específicas sobre o array
+
+// join Converte o array para uma String, juntando 
+// os elementos com base em um separador
+const ling = ['Java', 'Script']
+console.log(ling.join(''))
