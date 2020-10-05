871ad305db8cff65e876c6699a76ba0a823fe6b1
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-05 10:41:11 -0300

    usando includes

---
+++ array_accessor_2.js
@@ -0,0 +1,8 @@
+// Os accessor methods quando invocados retornam 
+// informações específicas sobre o array
+
+// includes Retorna true se o elemento existir
+const languages = ['Python', 'C', 'C', 'Java']
+console.log(languages.includes('C'))
+console.log(languages.includes('C'))
+console.log(languages.includes('JavaScript'))
