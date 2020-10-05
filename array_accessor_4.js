4affc3418e7b51b6ad0bcb0a8dbb6894fd917bf2
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-05 10:55:13 -0300

    uso de slice

---
+++ array_accessor_4.js
@@ -0,0 +1,7 @@
+// Os accessor methods quando invocados retornam 
+// informações específicas sobre o array
+
+// slice Retorna partes de um determinado array
+//  de acordo com a posição de início e fim
+const ling = 'JavaScript'
+console.log(ling.slice(0, 4))
